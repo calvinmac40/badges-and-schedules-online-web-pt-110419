1717e930ca2b2846db61f0ff273fe07e16869241
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-13 00:33:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,8 @@ def assign_rooms(attendees)
   new_array = []
   counter = 1 
   attendees.each do |name| 
-    new_array << "Hello, #{name}! You'll be assigned to room #{counter+1}!"
+    new_array << "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter +=1
   end
   new_array
 end
